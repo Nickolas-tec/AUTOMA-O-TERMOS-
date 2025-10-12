68c5a388f8ffc44912c599536266813654f8cfae
Nickolas <nickolasgomesduraes@outlook.com>
2025-10-11 23:26:20 -0300

    Termos Arquiteto de Software

--- script 02.py
+++ script 02.py
@@ -24,7 +24,7 @@ data_frame_2 = data_frame[filtro_colunas].copy()
 
 # === APLICANDO FILTRO DE PERFIL ===
 data_frame_2['Perfil 2025'] = data_frame_2['Perfil 2025'].str.strip().str.lower()
-perfil_desejado = 'especialista em tecnologias / negócios'
+perfil_desejado = 'arquiteto de software'
 data_frame_filtrado_final = data_frame_2[data_frame_2['Perfil 2025'] == perfil_desejado]
 
 # # === SALVANDO PLANILHA FILTRADA ===
